73706451d6b13da73635ea0ca5ba5eaffd2080ed
高伟 <wb-gw879379@koubei.com>
2021-07-29 17:24:58 +0800

    feat: firstTimeCommit

---
+++ Desktop/vue-preject/src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>goood</h1>
+        <el-button type="success" size="mini">别点我</el-button>
+        <el-button type="success" size="small">别点我</el-button>
+        <button>别点我</button>
+    </div>
+</template>
+<script>
+
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
